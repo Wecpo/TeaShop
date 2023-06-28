c5921063fdb4e26dc4012ac67d1c8cb0296c9698
Senin Maksim <wecpo@yandex.ru>
2023-06-28 11:56:28 +0300

    first deploy

--- client/src/layouts/logOut.jsx
+++ client/src/layouts/logOut.jsx
@@ -3,9 +3,11 @@ import { useDispatch } from "react-redux";
 import { logOut } from "../store/users";
 import { useNavigate } from "react-router";
 import Loader from "../components/ui/Loader";
+
 const LogOut = () => {
      const navigate = useNavigate();
      const dispatch = useDispatch();
+
      useEffect(() => {
           dispatch(logOut());
           localStorage.clear();
--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const chalk = require(`chalk`);
 const cors = require(`cors`);
 const initDatabase = require(`./startUp/initDatabase`);
 const routes = require(`./routes`);
-
+const path = require(`path`);
 const app = express();
 
 app.use(express.json());
@@ -16,11 +16,15 @@ app.use(`/api`, routes);
 
 const PORT = config.get(`port`) ?? 8080;
 
-// if (process.env.NODE_ENV === `production`) {
-//      console.log(`Production`);
-// } else {
-//      console.log(`Development`);
-// }
+if (process.env.NODE_ENV === "production") {
+     app.use("/", express.static(path.join(__dirname, "client")));
+
+     const indexPath = path.join(__dirname, "client", "index.html");
+
+     app.get("*", (req, res) => {
+          res.sendFile(indexPath);
+     });
+}
 
 async function start() {
      try {
